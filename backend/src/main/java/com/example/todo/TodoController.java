f4a0328a970f98a2e3079ebd13a2d47bb4ef4cba
fianerd <mattiasson.sofia@gmail.com>
2026-09-22 16:30:49 +0200

    feat(item-to-lis): add one more item to the list

--- backend/src/main/java/com/example/todo/TodoController.java
+++ backend/src/main/java/com/example/todo/TodoController.java
@@ -1,10 +1,8 @@
 package com.example.todo;
-
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RestController;
-
-	import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -13,9 +11,9 @@ public class TodoController {
 	@GetMapping
  public List<Todo> getTodos(){
 	return List.of( new Todo(1L, "Buy Milk", "Shop", "Get the light version", false),
-	 new Todo(2L, "Buy Chees", "Shop", "Gouda", false),
-	 new Todo(3L, "Buy Meat", "Shop", "Chicken", true),
-			new Todo(3L, "Make the dishes", "Home", "Fix in the kitchen", false)
+		 new Todo(2L, "Buy Chees", "Shop", "Gouda", false),
+		 new Todo(3L, "Buy Meat", "Shop", "Chicken", true),
+		 new Todo(3L, "Make the dishes", "Home", "Fix in the kitchen", false)
 	);
  }
 }
