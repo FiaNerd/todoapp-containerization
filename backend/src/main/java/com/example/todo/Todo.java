bbd61566cb23632677961328ac9f70609f43274e
fianerd <mattiasson.sofia@gmail.com>
2026-09-22 13:38:45 +0200

    feat(todo-app): add insallation of spring boot and add list of todos

--- backend/src/main/java/com/example/todo/Todo.java
+++ backend/src/main/java/com/example/todo/Todo.java
@@ -1,9 +1,5 @@
 package com.example.todo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import java.util.Objects;
 
 public class Todo {
