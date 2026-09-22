f4a639fb2dd2325614a651184e8b62540bae3486
fianerd <mattiasson.sofia@gmail.com>
2026-09-22 16:29:17 +0200

    feat(todo-app): add one more item to the list

--- backend/src/main/java/com/example/todo/TodoController.java
+++ backend/src/main/java/com/example/todo/TodoController.java
@@ -12,9 +12,10 @@ public class TodoController {
 
 	@GetMapping
  public List<Todo> getTodos(){
-	return List.of( new Todo(1L, "Buy Milk", "Shop", "Get the light verison", false),
+	return List.of( new Todo(1L, "Buy Milk", "Shop", "Get the light version", false),
 	 new Todo(2L, "Buy Chees", "Shop", "Gouda", false),
-	 new Todo(3L, "Buy Meat", "Shop", "Chicken", true)
+	 new Todo(3L, "Buy Meat", "Shop", "Chicken", true),
+			new Todo(3L, "Make the dishes", "Home", "Fix in the kitchen", false)
 	);
  }
 }
